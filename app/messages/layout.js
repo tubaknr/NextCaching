17c437cb89fad0d63c02c945cd89d0fd8495553c
Tuba Çulhaoğlu <tubaculhaoglu1@gmail.com>
2024-12-14 02:26:43 +0300

    Add cache details

--- app/messages/layout.js
+++ app/messages/layout.js
@@ -1,9 +1,26 @@
 export default async function MessagesLayout({ children }) {
-    const response = await fetch('http://localhot:8080/messages', {
-        headers: {
-            'X-ID': 'layout',
-        },
-    });
+    const response = await fetch('http://localhost:8080/messages', 
+        // {
+        // headers: { //configuration --> OLMAZSA: layout'daki ve page'deki fetch'lerin farkı kalmaz-->nextjs sadece 1 kere fetch eder. bu MEMOIZATION.
+        // //  şu anda 2 farklı fetch algılıyor, 2 farklı fetch yapıyor.
+        // // ama next15 de bu değiştirildi, her türlü 2 farklı fetch yapıyor.
+        //     'X-ID': 'layout',
+        // },
+
+    // }
+    {
+        // cache: 'force-catch', // next14 deki fetch memoization, MORE CACHING MORE EFFICIENCY.
+        cache: 'no-store' //dont cache it
+
+    },
+    {
+        next: {
+            // Getting some caching advantages but not caching forever.
+            revalidate: 5 // for 5 secs, use the cache then delete it.
+
+        }
+    }
+);
 
     const messages = await response.json();
     const totalMessages = messages.length;
